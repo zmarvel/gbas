fe00d23c85d96b8259045ef313d63ed16f51d575
Zack Marvel <zpmarvel@gmail.com>
2018-10-13 23:19:58 -0600

    Don't just tokenize a simple example, allow any input file

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,50 @@
 
 
+#include <fstream>
 #include "tokenizer.h"
 
+class InputFile {
+  public:
+    InputFile(std::string filename) : filename(filename) {
+      stream = std::ifstream{filename};
+    }
+
+    ~InputFile() {
+      stream.close();
+    }
+
+    bool exists() {
+      return stream.is_open();
+    }
+
+    std::ifstream &getStream() {
+      return stream;
+    }
+
+  private:
+    std::string filename;
+    std::ifstream stream;
+};
+
+static const std::string USAGE = " <input file>";
 int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << argv[0] << USAGE << std::endl;
+    return -1;
+  }
+
+  InputFile infile{std::string{argv[1]}};
+  if (!infile.exists()) {
+    std::cerr << "Input file does not exist" << std::endl;
+    return -1;
+  }
+
   auto tokenizer = Tokenizer{};
-  auto input = "add a, 32\n";
-  std::stringstream stream;
-  stream << input;
-  auto tokens = tokenizer.tokenize(stream);
+  auto tokens = tokenizer.tokenize(infile.getStream());
   std::cout << tokens->size() << std::endl;
-  std::cout << tokens->at(0)  << std::endl;
-  std::cout << tokens->at(1)  << std::endl;
-  std::cout << tokens->at(2)  << std::endl;
-  std::cout << tokens->at(3)  << std::endl;
+  for (auto it = tokens->begin(); it != tokens->end(); it++) {
+    std::cout << *it  << std::endl;
+  }
 
   return 0;
 }
